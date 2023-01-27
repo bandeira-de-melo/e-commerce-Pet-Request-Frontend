80af1006c9fbc0b1f8d0a0f2ed28a45b06102125
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-01-27 19:22:30 -0300

    feat: button component

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 46px;
+  border: none;
+  border-radius: 5px;
+  background: #A328D6;
+  cursor: pointer;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.15);
+
+  font-family: "Raleway";
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+  color: #FFFFFF;
+`
